88cf2ff1649e19d5edf6e1503ee106957af7f49b
Duck-dck <phamhungthinh23@gmail.com>
2026-04-09 01:58:17 +0700

    Update customer.js

--- lab_7-Copy/server/api/customer.js
+++ lab_7-Copy/server/api/customer.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Product = require('../models/Product');
+const Product = require('../models/Models');
 const router = express.Router();
 const {CustomerDAO, OrderDAO} = require('../models/CustomerDAO');
 const CryptoUtil = require('../utils/CryptoUtil');
